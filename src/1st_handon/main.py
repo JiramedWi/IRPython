74b42c7c4a4a34bee50f61634e9ef7f07d81114d
Jiramed <jiramed_wi@cmu.ac.th>
2021-11-18 17:40:35 +0700

    add count python and mysql counter

--- src/1st_handon/main.py
+++ src/1st_handon/main.py
@@ -7,7 +7,8 @@ def get_and_clean_data():
     description = data['job_description']
     cleaned_description = description.apply(lambda s: s.translate(str.maketrans('', '', string.punctuation + u'\xa0')))
     cleaned_description = cleaned_description.apply(lambda s: s.lower())
-    cleaned_description = cleaned_description.apply(lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+    cleaned_description = cleaned_description.apply(
+        lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
     cleaned_description = cleaned_description.drop_duplicates()
     return cleaned_description
 
@@ -25,3 +26,12 @@ def parse_job_description():
 
 if __name__ == 'main':
     cleaned_description = parse_job_description()
+
+
+def count_python_mysql():
+    parsed_description = parse_job_description()
+    count_python = parsed_description.apply(lambda s: 'python' in s).sum()
+    count_mysql = parsed_description.apply(lambda s: 'mysql' in s).sum()
+    print('python: ' + str(count_python) + ' of ' + str(parsed_description.shape[0]))
+    print('mysql: ' + str(count_mysql) + ' of ' + str(parsed_description.shape[0]))
+
