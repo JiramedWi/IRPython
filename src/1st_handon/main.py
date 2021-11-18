43f74573ace50311eb3f49461aa521c9500264be
Jiramed <jiramed_wi@cmu.ac.th>
2021-11-18 18:19:03 +0700

    done 1st on hand

--- src/1st_handon/main.py
+++ src/1st_handon/main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import string
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
 
 
 def get_and_clean_data():
@@ -35,3 +38,34 @@ def count_python_mysql():
     print('python: ' + str(count_python) + ' of ' + str(parsed_description.shape[0]))
     print('mysql: ' + str(count_mysql) + ' of ' + str(parsed_description.shape[0]))
 
+
+def parse_db():
+    html_doc = requests.get("https://db-engines.com/en/ranking").content
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    db_table = soup.find("table", {"class": "dbi"})
+    all_db = [''.join(s.find('a').findAll(text=True, recursive=False)).strip() for s in
+              db_table.findAll("th", {"class": "pad-l"})]
+    all_db = list(dict.fromkeys(all_db))
+    db_list = all_db[:10]
+    db_list = [s.lower() for s in db_list]
+    db_list = [[x.strip() for x in s.split()] for s in db_list]
+    return db_list
+
+
+def get_list_of_db():
+    parsed_description = parse_job_description()
+    cleaned_db = parse_db()
+
+    raw = [None] * len(cleaned_db)
+    for i, db in enumerate(cleaned_db):
+        raw[i] = parsed_description.apply(lambda s: np.all([x in s for x in db])).sum()
+        print(' '.join(db) + ': ' + str(raw[i]) + ' of ' + str(parsed_description.shape[0]))
+
+        with_python = [None] * len(cleaned_db)
+    for i, db in enumerate(cleaned_db):
+        with_python[i] = parsed_description.apply(lambda s: np.all([x in s for x in db]) and 'python' in s).sum()
+        print(' '.join(db) + ' + python: ' + str(with_python[i]) + ' of ' + str(parsed_description.shape[0]))
+
+    for i, db in enumerate(cleaned_db):
+        print(' '.join(db) + ' + python: ' + str(with_python[i]) + ' of ' + str(raw[i]) + ' (' +
+              str(np.around(with_python[i] / raw[i] * 100, 2)) + '%)')
